87a4745d00714de394cba6386f4b18749bd5d2b3
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2025-06-20 17:01:07 -0700

    uploaded complete project

---
+++ generator.py
@@ -0,0 +1,38 @@
+from faker import Faker
+import re
+
+fake = Faker()
+
+def generate_name():
+    found = False
+    while not found:
+        full_name = fake.name()
+        name_list = full_name.split(" ")
+        if len(name_list) == 2:
+            found = True
+    return name_list[0], name_list[1]
+
+def generate_number():
+    found = False
+    while not found:
+        number = fake.phone_number()
+        pattern = re.compile("[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]")
+        result = re.fullmatch(pattern, number)
+        if result:
+            number = number[0:3] + number[4:7] + number[8:]
+            found = True
+    return number
+
+def generate_email():
+    return fake.email()
+        
+def generate_contact():
+    contact = {}
+    first_name, last_name = generate_name()
+    number = generate_number()
+    email = generate_email()
+    contact["First Name"] = first_name
+    contact["Last Name"] = last_name
+    contact["Number"] = number
+    contact["Email"] = email
+    return contact
---
+++ main.py
@@ -0,0 +1,15 @@
+import generator
+import json
+
+def main():
+    contacts = []
+    for _ in range(10000):
+        contact = generator.generate_contact()
+        contacts.append(contact)
+    
+    with open("contacts.json", "w") as f:
+        json.dump(contacts, f, indent=4)
+        
+if __name__ == "__main__":
+    main()
+        
